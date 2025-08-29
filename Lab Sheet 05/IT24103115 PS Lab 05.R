0acd3fbcae7271eebd9f4943c6483240042a0a95
it24103115 <it24103115@my.sliit.lk>
2025-08-29 16:23:12 +0530

    It24103115 Lab 05

---
+++ Lab Sheet 05/IT24103115 PS Lab 05.R
@@ -0,0 +1,34 @@
+setwd("C:\\Users\\it24103115\\Desktop\\IT24103115")
+#Q1
+Delivery_Times <- read.table("Exercise - Lab 05.txt", header= TRUE)
+head(Delivery_Times)
+
+#Q2
+Delivery_Times$Delivery_Time_.minutes. <- as.numeric(Delivery_Times$Delivery_Time_.minutes.)
+breaks <- seq(20,70,length.out = 10)
+
+hist(Delivery_Times$Delivery_Time_.minutes.,
+     breaks = breaks,
+     right=TRUE,
+     main ="Histogram of Delivery Times",
+     xlab = "Delivery Times(minutes)",
+     ylab = "Frequency",
+     col = "lightblue",
+     border ="black")
+
+#Q3
+#It rises at first and then falls after the middle, which means it is symmetrical. 
+
+#Q4
+hist_data <- hist(Delivery_Times$Delivery_Time_.minutes.,
+                  breaks =breaks,
+                  plot = FALSE)
+cum_freq <- cumsum(hist_data$counts)
+
+plot(hist_data$breaks[-1],cum_freq,
+     type="o",
+     main="Cumulative Frequency polygon (Ogive)",
+     xlab = "Delivery Times(minutes",
+     ylab = "Cumulative Frequency",
+     col = "red")
+
